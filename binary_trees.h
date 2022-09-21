09b4427e6165833a6a943449d37bcf888c72c98e
walebign3 <walebign3@gmail.com>
2022-09-22 01:28:31 +0300

    0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,6 @@
+#ifndef BIN_TREE_H
+#define BIN_TREE_H
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,3 +20,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
+
+#endif
+
