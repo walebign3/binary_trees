8a296f2dcb488ab2fe0f1907a74826da713d557e
walebign3 <walebign3@gmail.com>
2022-09-22 20:35:14 +0300

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,11 +10,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		free(tree);
-		return;
-	}
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
+	free(tree);
 }
