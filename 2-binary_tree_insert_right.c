5e6ad35d37b8bd5d17336214d86bad5b725af2f7
walebign3 <walebign3@gmail.com>
2022-09-22 14:15:28 +0300

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *result;
 
 	if (parent == NULL)
-		return(NULL);
+		return (NULL);
 	result = malloc(sizeof(binary_tree_t));
 	if (result != NULL)
 	{
