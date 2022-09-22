ea3fcff1c73cad1b05f09aec33b7b25234cd09da
walebign3 <walebign3@gmail.com>
2022-09-22 09:12:44 +0300

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,7 +10,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t* result;
+	binary_tree_t *result;
 
 	result = malloc(sizeof(binary_tree_t));
 	if (result != NULL)
--- binary_trees.h
+++ binary_trees.h
@@ -16,10 +16,8 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif
-
