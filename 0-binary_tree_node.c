c2bf7003d206abfc247f7b215be524e6b3e5c805
walebign3 <walebign3@gmail.com>
2022-09-22 08:51:42 +0300

    binary_tree_print.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t* result;
 
 	result = malloc(sizeof(binary_tree_t));
-	if (result == NULL)
-		return (NULL);
-	result->parent = parent;
-	result->left = NULL;
-	result->right = NULL;
-	result->n = value;
+	if (result != NULL)
+	{
+		result->parent = parent;
+		result->left = NULL;
+		result->right = NULL;
+		result->n = value;
+	}
 	return (result);
 }
