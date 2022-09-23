0670d0f23abf0ff82f88246c47aea89ec95cf05c
walebign3 <walebign3@gmail.com>
2022-09-23 21:35:56 +0300

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,17 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 	if (tree != NULL && tree->left == NULL && tree->right == NULL)
 		return (1);
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
-        if (left_height >= right_height)
-                return (left_height + 1);
-        return (right_height + 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height >= right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
 /**
  * binary_tree_balance - balance factor of a tree
