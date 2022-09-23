44fb31a9b5ec73842bf3be2845410e53fd7f1ceb
walebign3 <walebign3@gmail.com>
2022-09-23 22:27:29 +0300

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,26 +1,6 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_height - in-order traversal
- * @tree: Pointer to the root node
- *
- * Return: height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL || tree->right == NULL)
-		return (1);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (left_height >= right_height)
-		return (left_height + 1);
-	return (right_height + 1);
-}
-/**
  * binary_tree_is_full - full binary tree
  * @tree: Pointer to the root node
  *
@@ -28,14 +8,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left_balance, right_balance;
-
 	if (tree == NULL)
 		return (0);
-	left_balance = binary_tree_height(tree->left);
-	right_balance = binary_tree_height(tree->right);
-	if ((left_balance - right_balance) == 0)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	return (0);
 }
 
