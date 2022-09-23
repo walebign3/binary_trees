3987a84f179b13ce82d58f64b0745bb6f2d05194
walebign3 <walebign3@gmail.com>
2022-09-23 15:03:15 +0300

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree != NULL && tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	if (tree != NULL && tree->left != NULL && tree->right == NULL)
 		return (1);
 	if (tree != NULL && tree->left == NULL && tree->right != NULL)
